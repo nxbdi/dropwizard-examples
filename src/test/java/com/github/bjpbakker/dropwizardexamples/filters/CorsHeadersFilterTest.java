899cac2c47c435bd18de45e0a64973b6eec9f9c2
Bart Bakker <bart@thesoftwarecraft.com>
2013-04-23 00:14:31 +0200

    Clean up CORS headers test cases.
    Use an embedded HTTP server for running the filter rather than mocking
    servlet request and response. Only mock what you own.
    
    This also removes the dependency on Spring framework for it includes
    mock implementations of http-servlet classes.

--- src/test/java/com/github/bjpbakker/dropwizardexamples/filters/CorsHeadersFilterTest.java
+++ src/test/java/com/github/bjpbakker/dropwizardexamples/filters/CorsHeadersFilterTest.java
@@ -1,48 +1,110 @@
 package com.github.bjpbakker.dropwizardexamples.filters;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-
-import org.junit.Assert;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHttpRequest;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.mock.web.MockFilterChain;
-import org.springframework.mock.web.MockFilterConfig;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
 
-/**
- * Tests the {@link CorsHeadersFilter} class.
- */
+import javax.servlet.*;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.EnumSet;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class CorsHeadersFilterTest {
+    private static final String HEADER_NOT_SET = null;
+    private static final HttpHost HTTP_HOST = new HttpHost("localhost", 59876);
 
-    private MockHttpServletRequest httpRequest = new MockHttpServletRequest();
-    private MockHttpServletResponse httpResponse = new MockHttpServletResponse();
-    private MockFilterChain filterChain = new MockFilterChain();
+    private Server server;
+    private CorsHeadersFilter subject;
 
-    @Test
-    public void itShouldAddCorsHeaderInGetRequest() throws ServletException, IOException {
-        CorsHeadersFilter filter = new CorsHeadersFilter();
-        filter.init(new MockFilterConfig());
-        httpRequest.setMethod("GET");
-        filter.doFilter(httpRequest, httpResponse, filterChain);
-        // expect header =  Access-Control-Allow-Origin:*
-        Assert.assertSame("Don't expect changed response object", httpResponse, filterChain.getResponse());
-        Assert.assertEquals("response header Access-Control-Allow-Origin for CORS is not set!", "*" , httpResponse.getHeaderValue("Access-Control-Allow-Origin"));
-        Assert.assertNull("response header Access-Control-Allow-Headers for CORS should not be set!", httpResponse.getHeaderValue("Access-Control-Allow-Headers"));
+    @Before
+    public void setUp() throws Exception {
+        subject = new CorsHeadersFilter();
+        server = bootstrapServerWithFilter(subject);
+        server.start();
     }
 
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
 
     @Test
-    public void itShouldAddCorsHeadersInOptionRequest() throws ServletException, IOException {
-        CorsHeadersFilter filter = new CorsHeadersFilter();
-        filter.init(new MockFilterConfig());
-        httpRequest.setMethod("OPTIONS");
-        filter.doFilter(httpRequest, httpResponse, filterChain);
-        // expect header =  Access-Control-Allow-Origin:*
-        // expect header =  Access-Control-Allow-Headers:Content-Type
-        Assert.assertSame("Don't expect changed response object", httpResponse, filterChain.getResponse());
-        Assert.assertEquals("response header Access-Control-Allow-Origin for CORS is not set!", "*" , httpResponse.getHeaderValue("Access-Control-Allow-Origin"));
-        Assert.assertEquals("response header Access-Control-Allow-Headers for CORS is not set!", "Content-Type" , httpResponse.getHeaderValue("Access-Control-Allow-Headers"));
+    public void it_adds_CORS_allow_origin_header_on_GET_response() throws Exception {
+        HttpResponse response = get("/");
+        assertThat(responseHeader("Access-Control-Allow-Origin", response), hasValue("*"));
+    }
+
+    @Test
+    public void it_leaves_CORS_allow_headers_header_unset_on_GET_response() throws Exception {
+        HttpResponse response = get("/");
+        assertThat(responseHeader("Access-Control-Allow-Headers", response), is(unset()));
+    }
+
+    @Test
+    public void it_adds_CORS_allow_origin_header_on_OPTIONS_response() throws Exception {
+        HttpResponse response = options("/");
+        assertThat(responseHeader("Access-Control-Allow-Origin", response), hasValue("*"));
+    }
+
+    @Test
+    public void it_adds_CORS_allow_headers_header_on_OPTIONS_response() throws Exception {
+        HttpResponse response = options("/");
+        assertThat(responseHeader("Access-Control-Allow-Headers", response), hasValue("Content-Type"));
+    }
+
+    private HttpResponse get(String resource) throws IOException {
+        return execute("GET", resource);
+    }
+
+    private HttpResponse options(String resource) throws IOException {
+        return execute("OPTIONS", resource);
+    }
+
+    private HttpResponse execute(String method, String resource) throws IOException {
+        HttpClient client = new DefaultHttpClient();
+        HttpRequest request = new BasicHttpRequest(method, resource);
+        return client.execute(HTTP_HOST, request);
+    }
+
+    private String responseHeader(String name, HttpResponse response) {
+        Header header = response.getFirstHeader(name);
+        return header != null ? header.getValue() : HEADER_NOT_SET;
+    }
+
+    private Matcher<? super Object> hasValue(Object value) {
+        return equalTo(value);
+    }
+
+    private Matcher<String> unset() {
+        return equalTo(HEADER_NOT_SET);
+    }
+
+    private Server bootstrapServerWithFilter(CorsHeadersFilter filter) {
+        Server server = new Server(new InetSocketAddress(HTTP_HOST.getHostName(), HTTP_HOST.getPort()));
+        ServletContextHandler root = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
+        root.addFilter(new FilterHolder(filter), "/*", EnumSet.of(DispatcherType.REQUEST));
+        root.addServlet(new ServletHolder(new EmptyResourceServlet()), "/*");
+        return server;
+    }
+
+    private static class EmptyResourceServlet extends GenericServlet {
+        @Override
+        public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {}
     }
 }
